bd50c9147ea88f3fb9404ab85a4a8cb9edc43e06
Milo Carbol <mcarbol@pingidentity.com>
2016-04-15 19:26:19 -0700

    Basic dice API

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dice API
+
+This rolls dice. It's cool.
+
+## Running Locally
+
+Requires Node.js. Go install it.
+
+```sh
+$ npm install
+$ npm start
+```
+
+It's now running on [localhost:5000](http://localhost:5000/).
---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+
+var notation = /^\d*d\d+(\+\d+)?$/
+
+function rollDie(count, die, add) {
+  console.log('Rolling ' + count + 'd' + die + '+' + add);
+  
+  var rolls = {};
+  var total = 0;
+  for (i = 0; i < count; i++) {
+    var roll = Math.floor(Math.random() * die) + 1;
+    rolls['die' + (i+1)] = roll;
+    total += roll;
+  }
+  rolls['total'] = total+add;
+  return rolls;
+}
+
+function parseRoll(roll) {
+  console.log('Parsing ' + roll);
+  
+  var split1 = roll.split('d');
+  var count = split1[0] || 1;
+  var dieAndAdd = split1[1].split('+');
+  var die = dieAndAdd[0];
+  var add = dieAndAdd[1] || 0;
+  
+  return {'count': parseInt(count), 'die': parseInt(die), 'add': parseInt(add)};
+}
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/roll', function(request, response) {
+  var roll = request.query.roll;
+  if (notation.test(roll)) {
+    var parsedRoll = parseRoll(roll);
+    var result = rollDie(parsedRoll.count, parsedRoll.die, parsedRoll.add);
+    
+    console.log('Returning result: ' + JSON.stringify(result));
+    response.json(result);
+  }
+  else {
+    response.sendStatus(400);
+  }
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
